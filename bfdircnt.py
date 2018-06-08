0704ff56c08a547fb32ffba2921b6e14370def3f
Pete Schmitt <peter.r.schmitt@icloud.com>
2018-06-08 13:53:17 -0400

    new program added

---
+++ bfdircnt.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#===============================================================================
+#
+#          FILE:  bfdircnt.py
+# 
+#         USAGE:  bfdircnt <directory> 
+# 
+#   DESCRIPTION:  bftree like on Linux directories  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  python 2.7.x
+#          BUGS:  ---
+#       UPDATES:  ---
+#        AUTHOR:  Pete Schmitt (discovery), pschmitt@upenn.edu
+#       COMPANY:  University of Pennsylvania
+#       VERSION:  0.1.0
+#       CREATED:  Fri Jun  8 13:37:59 EDT 2018
+#      REVISION:  ---
+#===============================================================================
+import copy
+import os
+import sys
+import collections
+###############################################################################
+def printf(format, *args):
+    """ works just like the C/C++ printf function """
+    import sys
+    sys.stdout.write(format % args)
+    sys.stdout.flush()
+###############################################################################
+def traverse(path):
+    tree = dict()
+    for dirName, subdirList, fileList in os.walk(path):
+        tree[dirName] = [len(subdirList), len(fileList)]
+
+    ntree = copy.deepcopy(tree)
+    return tree, collections.OrderedDict(sorted(ntree.items()))
+###############################################################################
+try:
+    path = sys.argv[1]
+except:
+    printf("Syntax: bfdircnt <directory>\n")
+    sys.exit(1)
+
+if path[-1] == '/':
+    path = path[0:-1]
+
+if os.path.exists(path) and os.path.isdir(path):
+    BASE = os.path.basename(path)
+    DIR  = os.path.dirname(path)
+    os.chdir(path + '/..')
+else:
+    printf("Error: argument must be existing directory\n")
+    sys.exit(2)
+
+atree, otree = traverse(BASE)
+
+for a in otree:
+    for b in atree:
+        if a in b and a != b:
+            otree[a][0] += atree[b][0]
+            otree[a][1] += atree[b][1]
+
+tab = '\t'
+for d in otree:
+    dirs = otree[d][0]
+    files = otree[d][1]
+    dlist = d.split('/')
+    slashes = len(dlist) - 1
+    if slashes > 0:
+        printf("%s", "    " * slashes)
+    printf("%s (dirs: %d files: %d)\n", os.path.basename(d), dirs, files)
